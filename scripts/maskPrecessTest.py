bbe5e54bcfbaf16e622188bef9d39c5ae925ae26
Malthe Have Musaeus <malthehave@gmail.com>
2023-04-23 14:44:01 +0200

    Added a simple nn for detecting if a mask is a ne

--- scripts/maskPrecessTest.py
+++ scripts/maskPrecessTest.py
@@ -1,17 +1,17 @@
 #test the bert ner model from huggingface
 from transformers import AutoTokenizer, AutoModelForTokenClassification
 from transformers import pipeline
-from helpers import load_conll
+# from helpers import load_conll
 import pandas as pd
 from datasets import load_dataset
 
 #preprocess dataz
 
-wnut = load_dataset("wnut_17")
+# wnut = load_dataset("wnut_17")
 
 
 #preprocess data
-def preprocess(data):
+def generate_masked_sentences(data):
     #creates new dataset where each token in a sentence is masked and used as a new sentence
     #the ner tag of the masked token is used as the label
 
@@ -24,7 +24,7 @@ def preprocess(data):
             new_data.append({'id': id_,
                              'tokens': new_sentence,
                              'is_ner': bool(sentence['ner_tags'][i]),
-                             'ner_tags': sentence['ner_tags'][i],})
+                             'ner_tag': sentence['ner_tags'][i],})
             id_ += 1
 
     return new_data
